43ee1d2c1ae1612d52dd7fc457abfb4e3ff7befd
wqh872081365 <wangqihui0324@gmail.com>
2016-06-26 23:17:30 +0800

    功能开发

---
+++ dongman/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Music',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('singer', models.CharField(max_length=100)),
+                ('compose', models.CharField(max_length=100)),
+                ('album', models.CharField(max_length=200)),
+                ('time', models.CharField(max_length=100)),
+                ('source', models.CharField(max_length=400)),
+                ('label', models.CharField(max_length=500)),
+                ('mark', models.CharField(max_length=100)),
+                ('length', models.CharField(max_length=100)),
+                ('lyrics', models.TextField()),
+                ('url', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('listener', models.TextField()),
+                ('comment', models.TextField()),
+            ],
+        ),
+    ]
--- dongman/models.py
+++ dongman/models.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
+
+
+class Music(models.Model):
+    name = models.CharField(max_length=100)  # 歌名
+    singer = models.CharField(max_length=100)  # 歌手
+    compose = models.CharField(max_length=100)  # 作曲
+    album = models.CharField(max_length=200)  # 专辑
+    time = models.CharField(max_length=100)  # 发行时间
+    source = models.CharField(max_length=400)  # 出处
+    label = models.CharField(max_length=500)  # 标签
+    mark = models.CharField(max_length=100)  # 喜欢 收藏 下载
+    length = models.CharField(max_length=100)  # 时长
+    lyrics = models.TextField()  # 歌词
+    url = models.CharField(max_length=200)  # 网易云链接
+    description = models.TextField()  # 备注
+    listener = models.TextField()  # 听者
+    comment = models.TextField()  # 评论
+
